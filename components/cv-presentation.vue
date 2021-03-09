7b8ad35e343f9409875f3c781cd97151f62f5c1b
Josu <soleguia@gmail.com>
2021-03-09 07:53:44 +0100

    Añadido plugin para svgs

--- components/cv-presentation.vue
+++ components/cv-presentation.vue
@@ -2,23 +2,38 @@
     <header class="presentation">
         <h1>Josu Soleguia</h1>
         <h2>Desarrollador Front End</h2>
+        <div class="presentation__face" v-html="beardFace"></div>
     </header>
 </template>
 <script>
+import beardFace from "~/assets/svg/face_12.svg?raw";
 export default {
-
+    data(){
+        return {
+            beardFace
+        }
+    },
+    created(){
+        console.log(beardFace)
+    }
 }
 </script>
 
 <style scoped>
     .presentation {
         position: relative;
-        display: inline-flex;
-        flex-wrap: wrap;
+        display: grid;
+        grid-columns: 1fr 40%;
+        grid-rows: min-content;
     }
 
     .presentation h1,
     .presentation h2 {
+        grid-column: 1;
         width: 100%;
+    }
+    .presentation__face {
+        grid-column: 2;
+        grid-row: 1 / span 2;
     }
 </style>
